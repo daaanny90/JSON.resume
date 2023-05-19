f768b11e25b59dec746314d30d40676ceaf82783
Danny Spina <danny.spina@hotmail.it>
2023-05-19 14:27:44 +0200

    fix: change hardcoded name with name in config

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ const side_projects = config.side_projects.map((project) => {
 
 const Resume = () => (
   <PDFViewer width="100%" height="1000px">
-    <Document author="Danny Spina">
+    <Document author={config.name}>
       <Page style={styles.body}>
         {/* Header */}
         <View style={styles.headerContainer}>
